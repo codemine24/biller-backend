7e3b54138d5d1171bd3f1021168ffc445e33ea2a
codemine24 <codemine24@gmail.com>
2025-12-16 00:59:12 +0600

    feat: add vercel frontend URL to CORS allowed origins

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookiePerser());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://biller-frontend.vercel.app"],
     credentials: true,
   })
 );
